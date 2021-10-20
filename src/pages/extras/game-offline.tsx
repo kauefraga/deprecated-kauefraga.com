56165a8ac6d93061fc0725f92ed51eba9f9053b6
kauefraga <kauefragarodrigues456@gmail.com>
2021-10-20 11:58:25 -0300

    :bug: fix

---
+++ src/pages/extras/game-offline.tsx
@@ -0,0 +1,11 @@
+import { Flex, Heading, Link, ListItem, UnorderedList } from '@chakra-ui/react'
+import Container from '../../components/Container'
+
+
+export default function GameOffline({ }) {
+  return (
+    <Container>
+      <Heading as="h1" color="gray.300">Hello world</Heading>
+    </Container>
+  )
+}
