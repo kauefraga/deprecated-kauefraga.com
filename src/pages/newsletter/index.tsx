48739d9be1da88f649b37b56933fd0d8502dcb70
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2022-01-03 04:16:16 -0300

    :mag: chore: improve seo with <head>...

--- src/pages/newsletter/index.tsx
+++ src/pages/newsletter/index.tsx
@@ -1,6 +1,7 @@
 import React, { FormEvent, useState } from 'react'
-import { Flex, Heading, Button, Text } from '@chakra-ui/react'
+import Head from 'next/head'
 import axios from 'axios'
+import { Flex, Heading, Button, Text } from '@chakra-ui/react'
 import FlexContainer from '../../components/Container'
 import EmailInput from '../../components/EmailInput'
 
@@ -14,42 +15,47 @@ export default function Newsletter({ }) {
   }
 
   return (
-    <FlexContainer color="gray.300">
-      <Flex
-        as="form"
-        onSubmit={handleSignUpToNewsletter}
-        backgroundColor="gray.700"
-        borderRadius="md"
-        flexDir="column"
-        alignItems="stretch"
-        padding={8}
-        marginTop={4}
-        width="100%"
-        maxW="400px"
-      >
-        <Heading textAlign="center" color="purple.500" marginBottom={4}>Inscreva-se na newsletter</Heading>
-
-        <Text textAlign="center" fontSize="sm" color="gray.400" marginBottom={4}>
-          Assine a newsletter do Kauê Fraga
-        </Text>
-
-        <EmailInput
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-        />
-
-        <Button
-          type="submit"
-          backgroundColor="purple.500"
-          height="50px"
-          borderRadius="sm"
-          marginTop={6}
-          _hover={{ backgroundColor: 'purple.600' }}
+    <>
+      <Head>
+        <meta name="description" content="This is a simple newsletter"/>
+        <title>Newsletter | Kauê Fraga</title>
+      </Head>
+      <FlexContainer color="gray.300">
+        <Flex
+          as="form"
+          onSubmit={handleSignUpToNewsletter}
+          backgroundColor="gray.700"
+          borderRadius="md"
+          flexDir="column"
+          alignItems="stretch"
+          padding={8}
+          marginTop={4}
+          width="100%"
+          maxW="400px"
         >
-          INSCREVER
-        </Button>
-      </Flex>
-    </FlexContainer>
+          <Heading textAlign="center" color="purple.500" marginBottom={4}>Inscreva-se na newsletter</Heading>
 
+          <Text textAlign="center" fontSize="sm" color="gray.400" marginBottom={4}>
+            Assine a newsletter do Kauê Fraga
+          </Text>
+
+          <EmailInput
+            value={email}
+            onChange={event => setEmail(event.target.value)}
+          />
+
+          <Button
+            type="submit"
+            backgroundColor="purple.500"
+            height="50px"
+            borderRadius="sm"
+            marginTop={6}
+            _hover={{ backgroundColor: 'purple.600' }}
+          >
+            INSCREVER
+          </Button>
+        </Flex>
+      </FlexContainer>
+    </>
   )
 }
