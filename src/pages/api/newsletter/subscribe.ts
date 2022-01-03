59468142eb48d388a95558dd4db59bb89f39d1db
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2022-01-03 04:36:51 -0300

    :recycle: chore: change .env, improve response 200 message

--- src/pages/api/newsletter/subscribe.ts
+++ src/pages/api/newsletter/subscribe.ts
@@ -5,15 +5,13 @@ import connectToDatabase from '../../../services/databaseConnection'
 export default async (request: NextApiRequest, response: NextApiResponse) => {
   const { email } = request.body
 
-  const db = await connectToDatabase('newsletter', process.env.MONGODB_NEWSLETTER_URI)
+  const db = await connectToDatabase('newsletter', process.env.MONGODB_NEWSLETTER)
   const collection = db.collection('subscribers')
 
-  email.toLowerCase()
   await collection.insertOne({
-    email,
-    subscribeAt: new Date()
+    email: email.toLowerCase(),
+    createdAt: new Date()
   })
 
-  // console.log('[test] Emails has been inserted')
-  response.status(201).json({ ok: true })
+  response.status(201).json({ message: "Email has been inserted" })
 }
